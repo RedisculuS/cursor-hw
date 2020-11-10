696194c48a7bd58e2e434956693bf486cef72fec
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2020-11-10 22:56:28 +0200

    first homework and first try

---
+++ HW-1-Variables-and-Types/script-hw-1.js
@@ -0,0 +1,21 @@
+const chocolateBar = 15.678;
+const chedderCheese = 123.965;
+const feltTipPenPack = 90.2345;
+/*
+//max value
+console.log(Math.max(chocolateBar , chedderCheese , feltTipPenPack));
+
+//min value
+console.log(Math.min(chedderCheese , chocolateBar , feltTipPenPack));
+
+const sumOfProducts = chedderCheese + chocolateBar + feltTipPenPack;
+console.log(sumOfProducts);
+
+const chocolateBarWithoutCoins = Math.floor(chocolateBar);
+const chedderCheeseWithoutCoins = Math.floor(chedderCheese);
+const feltTipPenPackWithoutCoins = Math.floor(feltTipPenPack);
+const sumOfProductsWithoutCoins = chedderCheeseWithoutCoins + chocolateBarWithoutCoins + feltTipPenPackWithoutCoins;
+console.log(sumOfProductsWithoutCoins); 
+
+console.log(Math.round(sumOfProducts/100)*100);
+*/
