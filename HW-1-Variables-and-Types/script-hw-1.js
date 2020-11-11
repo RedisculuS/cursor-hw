16a08ca21fac2ec95d6e67e144ea9d1c35bb9307
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2020-11-11 11:10:58 +0200

    hw is done without great visual part

--- HW-1-Variables-and-Types/script-hw-1.js
+++ HW-1-Variables-and-Types/script-hw-1.js
@@ -1,20 +1,37 @@
-const chocolateBar = 15.678;
-const chedderCheese = 123.965;
-const feltTipPenPack = 90.2345;
-/*
-//max value
-console.log(Math.max(chocolateBar , chedderCheese , feltTipPenPack));
+const chocolatePrice = 15.678;
+const chedderPrice = 123.965;
+const penPrice = 90.2345;
 
-//min value
-console.log(Math.min(chedderCheese , chocolateBar , feltTipPenPack));
-*/
-const sumOfProducts = chedderCheese + chocolateBar + feltTipPenPack;
-console.log(sumOfProducts);
+console.log(Math.max(chocolatePrice , chedderPrice , penPrice));
 
-const chocolateBarWithoutCoins = Math.floor(chocolateBar);
-const chedderCheeseWithoutCoins = Math.floor(chedderCheese);
-const feltTipPenPackWithoutCoins = Math.floor(feltTipPenPack);
-const sumOfProductsWithoutCoins = chedderCheeseWithoutCoins + chocolateBarWithoutCoins + feltTipPenPackWithoutCoins;
-console.log(sumOfProductsWithoutCoins); 
+console.log(Math.min(chedderPrice , chocolatePrice , penPrice));
 
-console.log(Math.round(sumOfProducts/100)*100);
+const sumOfProductsPrices = chedderPrice + chocolatePrice + penPrice;
+console.log(sumOfProductsPrices);
+
+const chocolatePriceNoCoins = Math.floor(chocolatePrice);
+const chedderPriceNoCoins = Math.floor(chedderPrice);
+const penPriceNoCoins = Math.floor(penPrice);
+const sumOfProductsPricesNoCoins = chedderPriceNoCoins + chocolatePriceNoCoins + penPriceNoCoins;
+console.log(sumOfProductsPricesNoCoins); 
+
+console.log(Math.round(sumOfProductsPrices / 100) * 100);
+
+const isEven = sumOfProductsPricesNoCoins % 2 === 0;
+console.log(isEven);
+
+const clientMoney = 500;
+const clientChange = clientMoney - sumOfProductsPrices;
+console.log(clientChange);
+
+const averagePrice = Number((sumOfProductsPrices / 3).toFixed(2));
+console.log(averagePrice);
+
+const randomSale = Math.floor(Math.random() * 100) + 1;
+console.log(randomSale);
+const clientSaleToPay = Number((sumOfProductsPrices - (sumOfProductsPrices / 100 * randomSale)).toFixed(2));
+console.log(clientSaleToPay);
+
+const productsCostPrice = sumOfProductsPrices / 2;
+const cleanIncome = Number((productsCostPrice - (sumOfProductsPrices / 100 * randomSale)).toFixed(2));
+console.log(cleanIncome); 
