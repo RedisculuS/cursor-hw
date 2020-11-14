4e95520cdb47dfa575dc0bc7f14776c3298c1764
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2020-11-14 22:14:28 +0200

    hope it is done

--- HW-2-Loops-and_Conditions/js/script-hw-2.js
+++ HW-2-Loops-and_Conditions/js/script-hw-2.js
@@ -1,13 +1,31 @@
-const minNum = Number(prompt("Введіть число з якого будемо починати розрахунок"));
-const maxNum = Number(prompt("Введіть кінцеве число розрахунку"));
+let minNum;
+let maxNum;
 
-if (Number.isInteger(minNum)) {    
-}else{
-    minNum = prompt("Введіть інше число");
+do {
+    minNum = Number(prompt("Введіть число з якого будемо починати розрахунок"));
+} while (!Number.isInteger(minNum));
+
+do {
+    maxNum = Number(prompt("Введіть число до якого будемо складати"));
+} while (!Number.isInteger(maxNum));
+
+let needOddNum;
+needOddNum = confirm("Потрібно пропускати парні числа при обчисленні суми?");
+
+let sum = 0;
+
+if (needOddNum == 0) {
+    for (minNum; minNum <= maxNum; minNum++) {
+        sum += minNum;
+    }
+    console.log(`Сумма чисел з ${minNum} до ${maxNum} дорівнює : ${sum}`);
+} else { 
+    for (minNum; minNum <= maxNum; minNum++) {
+        if (minNum % 2 === 0) {
+            continue;
+        }else{
+            sum += minNum;
+        }
+    }
+    console.log(`Сумма чисел з ${minNum} до ${maxNum} без підрахунку пірних чисел дорівнює : ${sum}`);
 }
-
-if (Number.isInteger(maxNum)) {    
-}else{
-    maxNum = prompt("Введіть інше число");
-}
-
