5b03cff66bb7c68913ac1ca0f22cd745aadebd65
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2020-11-14 21:19:03 +0200

    second hw

---
+++ HW-2-Loops-and_Conditions/js/script-hw-2.js
@@ -0,0 +1,13 @@
+const minNum = Number(prompt("Введіть число з якого будемо починати розрахунок"));
+const maxNum = Number(prompt("Введіть кінцеве число розрахунку"));
+
+if (Number.isInteger(minNum)) {    
+}else{
+    minNum = prompt("Введіть інше число");
+}
+
+if (Number.isInteger(maxNum)) {    
+}else{
+    maxNum = prompt("Введіть інше число");
+}
+
