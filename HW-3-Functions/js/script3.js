17850e7a399abada6cff4d8ea6d7962b48ecd957
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2020-11-22 20:36:07 +0200

    hw after  first verifying

--- HW-3-Functions/js/script3.js
+++ HW-3-Functions/js/script3.js
@@ -1,12 +1,13 @@
 //1
 function getMaxDigit (number) {
+    let numberFunc = number;
     let max = 0;
     do {
-        if ((number % 10) > max) {
-            max = number % 10;
+        if ((numberFunc % 10) > max) {
+            max = numberFunc % 10;
         }
-        number = (number - (number % 10)) / 10;
-    } while(number > 0) {}
+        numberFunc = (numberFunc - (numberFunc % 10)) / 10;
+    } while(numberFunc > 0) {}
     return max;
 }
 
@@ -14,16 +15,19 @@ function getMaxDigit (number) {
 
 //2
 function getPow (number , power) {
-    let powerNumber = number;
-    for (let i = 1; i < Math.abs(power); i++) {
-        powerNumber *= number;
+    let numFunc = number;
+    let powerFunc = power;
+    for (let i = 1; i < Math.abs(powerFunc); i++) {
+        numFunc *= numFunc;
     }
-    if (power < 0) {
-        powerNumber = 1 / (powerNumber);
+    if (powerFunc < 0) {
+        numFunc = 1 / (numFunc);
+    } else if (powerFunc === 0){
+        numFunc = 1;
     }
-    return powerNumber;
+    return numFunc;
 }
-//console.log(getPow(-9,-5));
+// console.log(getPow(9,0));
 //console.log(Math.pow(-9,-5));
 
 //3
@@ -32,9 +36,7 @@ const formatName = (name) => name[0].toUpperCase() + name.slice(1).toLowerCase()
 
 //4
 //const cleanSalary = (salary) => salary *= 0.805;
-function cleanSalary (salary , tax = 19.5) {
-    return salary - (salary * (tax / 100));
-}
+const cleanSalary = (salary , tax = 19.5) => salary - (salary * (tax / 100));
 // console.log(cleanSalary(1000));
 
 //5
@@ -46,10 +48,12 @@ function getRandomNumber (beginNum, endNum) {
 
 //6
 function countLetter (letter , word) {
-    word = word.toLowerCase();
+    let wordFunc = word;
+    let letterFunc = letter;
+    wordFunc = wordFunc.toLowerCase();
     let count = 0;
-    for (let i = 0; i < word.length; i ++) {
-        if(word[i] === letter) {
+    for (let i = 0; i < wordFunc.length; i ++) {
+        if(wordFunc[i] === letterFunc) {
             count ++;
         } 
     }
@@ -59,29 +63,31 @@ function countLetter (letter , word) {
 
 //7
 function convertCurrency (moneyToConvert) {
-    let dollarBuy = 0.035;
-    let dollarSell = 28.36
-    moneyToConvert = moneyToConvert.toLowerCase();
-    if (moneyToConvert.includes("uan")) {
-        moneyToConvert = parseInt(moneyToConvert);
-        moneyToConvert *= dollarBuy;
-    } else if (moneyToConvert.includes("$")){
-        moneyToConvert = parseInt(moneyToConvert);
-        moneyToConvert *= dollarSell;
+    let converMoneytInFunc = moneyToConvert;
+    const dollarBuy = 0.035;
+    const dollarSell = 28.36
+    converMoneytInFunc = converMoneytInFunc.toLowerCase();
+    if (converMoneytInFunc.includes("uah")) {
+        converMoneytInFunc = parseInt(converMoneytInFunc);
+        converMoneytInFunc *= dollarBuy;
+    } else if (converMoneytInFunc.includes("$")){
+        converMoneytInFunc = parseInt(converMoneytInFunc);
+        converMoneytInFunc *= dollarSell;
     } else {
        return alert("Error, unsuitable currency");
     }
-    return moneyToConvert.toFixed(2);
+    return +converMoneytInFunc.toFixed(2);
 }
-//console.log(convertCurrency("100$"));
+// console.log(convertCurrency("100$"));
 
 //8
 function getRandomPassword (passwordLength = 8) {
+    let passLenFunc = passwordLength;
     let password = "";
-    if (passwordLength <= 0 ) {
-        passwordLength = 8;
+    if (passLenFunc <= 0 ) {
+        passLenFunc = 8;
     }
-    for (let i = 0; i < passwordLength; i++) {
+    for (let i = 0; i < passLenFunc; i++) {
         password += getRandomNumber(0,9);
     }
     return password;
@@ -90,11 +96,13 @@ function getRandomPassword (passwordLength = 8) {
 
 //9
 function deleteLetters (letter , word) {
-    //word = word.toLowerCase();
+    let wordDelete = word;
+    let letterDelete = letter;
+    wordDelete = wordDelete.toLowerCase();
     let newWord = "";
-    for (let i = 0; i < word.length; i ++) {
-        if(word[i] === letter) {
-            newWord = word.split([letter]).join("");
+    for (let i = 0; i < wordDelete.length; i ++) {
+        if(wordDelete[i] === letterDelete) {
+            newWord = wordDelete.split([letterDelete]).join("");
         }
     }
     return newWord;
@@ -102,17 +110,18 @@ function deleteLetters (letter , word) {
 //console.log(deleteLetters("a", "Mariia"));
 
 //10
-const isPalyndrom = (word) => word === word.split('').reverse().join('');
+const isPalyndrom = (word) => word.replaceAll(" ","") === word.replaceAll(" ","").split('').reverse().join('');
 //console.log(isPalyndrom('кокос'));
 //console.log(isPalyndrom('мадам'));
 
 //11
 function deleteDuplicateLetter (sentense) {
-    sentense = sentense.toLowerCase();
+    let sentenceFunc = sentense;
+    sentenceFunc = sentenceFunc.toLowerCase();
     let count = '';
-    for (let i = 0; i < sentense.length; i ++) {
-        if(sentense.lastIndexOf(sentense[i]) === sentense.indexOf(sentense[i])) {
-            count += sentense[i];
+    for (let i = 0; i < sentenceFunc.length; i ++) {
+        if(sentenceFunc.lastIndexOf(sentenceFunc[i]) === sentenceFunc.indexOf(sentenceFunc[i])) {
+            count += sentenceFunc[i];
         } 
     }
     return count;
@@ -126,8 +135,8 @@ document.writeln(`Функція №3: ${formatName("ИнноКЕНтиЙ")} <br
 document.writeln(`Функція №4: ${cleanSalary(1000, 25)} <br>`);
 document.writeln(`Функція №5: ${getRandomNumber(1, 10)} <br>`);
 document.writeln(`Функція №6: ${countLetter("а","Асталавіста")} <br>`);
-document.writeln(`Функція №7: ${convertCurrency("100$")} та ${convertCurrency("100UAN")} <br>`);
+document.writeln(`Функція №7: ${convertCurrency("100$")} та ${convertCurrency("100UAH")} <br>`);
 document.writeln(`Функція №8: ${getRandomPassword(5)} <br>`);
 document.writeln(`Функція №9: ${deleteLetters("a", "Mariia")} <br>`);
-document.writeln(`Функція №10: ${isPalyndrom('мадам')} та ${isPalyndrom('кокос')} <br>`);
+document.writeln(`Функція №10: ${isPalyndrom('мадам мадам')} та ${isPalyndrom('кокос')} <br>`);
 document.writeln(`Функція №11: ${deleteDuplicateLetter('Бісквіт був дуже ніжним')}`);
